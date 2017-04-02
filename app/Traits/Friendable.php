3a6be81f44933992178f9e1fee252c9e9e463ba9
polodev <polodev10@gmail.com>
2017-04-02 17:39:00 +0600

    #10, added add_friend and accept_friend function inside Friendable trait

--- app/Traits/Friendable.php
+++ app/Traits/Friendable.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Traits;
+use App\Friendship;
 
 /**
  * summary
@@ -9,6 +10,27 @@ trait Friendable
     public function hello()
     {
       return 'hello world';
+    }
+    public function add_friend($requester_id){
+      $friendship = Friendship::create([
+          'requester' => $requester_id,
+          'user_requested' => $this->id
+        ]);
+      if($friendship){
+        return response()->json($friendship, 200);
+      }
+        return response()->json('fail', 501);
+    }
+    public function accept_friend($requester_id){
+      $friendship = Friendship::where('requester', $requester_id)
+                      ->where('user_requested', $this->id)->first();
+      if ($friendship) {
+        $friendship->update([
+            'status' => 1
+          ]);
+        return response()->json($friendship, 200);
+      }
+      return response()->json('fail', 501);
     }
 }
 
