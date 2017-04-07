1c2dbe6243a256eb1f44205c32f46eb66a329e90
polodev <polodev10@gmail.com>
2017-04-07 11:15:41 +0600

    #29 added vuex store

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,11 +11,13 @@ require('./bootstrap');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+ import {store} from './store.js'
 
 Vue.component('example', require('./components/Example.vue'));
 Vue.component('profile', require('./components/profiles/Profiles.vue'));
 Vue.component('notification', require('./components/notification.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    store
 });
