c7ce5c4802e0d80ceb26ef65de932806fff77ab9
polodev <polodev10@gmail.com>
2017-04-08 06:41:41 +0600

    #37 store post in posts table

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
   public function store(Request $request){
-    return $request->all();
+    return Post::create([
+      'user_id' => auth()->id(),
+      'content' => $request->content
+    ]);
   }
 }
